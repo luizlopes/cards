d6ca609a61abca9833d6b8e42f99d38fc3c32df8
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-06-20 22:30:06 -0300

    ajustando busca_card

--- card.rb
+++ card.rb
@@ -7,6 +7,10 @@ class Card
     @ingles = ingles
   end
 
+  def include?(texto)
+    @portugues.upcase == texto.upcase || @ingles.upcase == texto.upcase
+  end
+
   def to_s
     "Portugues: #{@portugues} -> Ingles: #{@ingles}"
   end
