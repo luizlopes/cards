5e7a650a1acefb5a5d7c73c92889646552169dcc
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-06-25 09:48:39 -0300

    criando construtor para classe EmergencialCard

--- emergencial_card.rb
+++ emergencial_card.rb
@@ -1,3 +1,11 @@
+require 'card'
+
 class EmergencialCard < Card
   attr_reader :emergency_phone
+
+  def initialize(portugues, ingles, phone="nenhum")
+    super portugues, ingles
+    @emergency_phone = phone
+  end
+
 end
